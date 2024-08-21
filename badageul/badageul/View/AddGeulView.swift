b19cb218dda8f910287911eb61a56e3bbcb0313e
huiinny <134281095+huiinny@users.noreply.github.com>
2024-08-21 20:46:15 +0900

    add crud
    crud 구현

--- badageul/badageul/View/AddGeulView.swift
+++ badageul/badageul/View/AddGeulView.swift
@@ -1,32 +1,34 @@
 import SwiftUI
 
 struct AddGeulView: View {
-    @Binding var path: NavigationPath
     @Binding var geul: [GeulInfo]  // 상위 뷰로부터 전달된 글 리스트에 바인딩
-
+    
     @State private var selectedCategory: GeulInfo.Category? = nil
     @State private var title: String = ""
     @State private var name: String = ""
     @State private var content: String = ""
+    @Environment(\.presentationMode) var presentationMode  // 현재 뷰를 닫기 위해 사용
 
     var body: some View {
-        VStack(spacing: 20) {
+        VStack(alignment: .leading, spacing: 20) {
             Spacer()
             
             Text("카테고리")
                 .font(.headline)
+                .padding(.leading)
             
             HStack(spacing: 20) {
                 Button(action: {
                     selectedCategory = .book
                 }) {
                     Text("책")
+                        .foregroundColor(.black)
                         .padding()
-                        .background(selectedCategory == .book ? Color.gray.opacity(0.2) : Color.clear)
-                        .cornerRadius(8)
+                        .background(selectedCategory == .book ? Color("yellow") : Color.clear)
+                        .cornerRadius(25)
                         .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.black, lineWidth: 1)
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(Color(red: 0.48, green: 0.49, blue: 0.50), lineWidth: 1) // 테두리 색상 변경
                         )
                 }
                 
@@ -34,12 +36,13 @@ struct AddGeulView: View {
                     selectedCategory = .lyrics
                 }) {
                     Text("노래")
+                        .foregroundColor(.black)
                         .padding()
-                        .background(selectedCategory == .lyrics ? Color.gray.opacity(0.2) : Color.clear)
-                        .cornerRadius(8)
+                        .background(selectedCategory == .lyrics ? Color("orange") : Color.clear)
+                        .cornerRadius(25)
                         .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.black, lineWidth: 1)
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(Color(red: 0.48, green: 0.49, blue: 0.50), lineWidth: 1) // 테두리 색상 변경
                         )
                 }
                 
@@ -47,17 +50,19 @@ struct AddGeulView: View {
                     selectedCategory = .quote
                 }) {
                     Text("명언")
+                        .foregroundColor(.black)
                         .padding()
-                        .background(selectedCategory == .quote ? Color.gray.opacity(0.2) : Color.clear)
-                        .cornerRadius(8)
+                        .background(selectedCategory == .quote ? Color("green") : Color.clear)
+                        .cornerRadius(25)
                         .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.black, lineWidth: 1)
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(Color(red: 0.48, green: 0.49, blue: 0.50), lineWidth: 1) // 테두리 색상 변경
                         )
                 }
             }
+            .padding(.leading)
             
-            VStack(alignment: .leading, spacing: 10) {
+            VStack(alignment: .leading, spacing: 20) {
                 Text("제목")
                     .font(.headline)
                 
@@ -65,46 +70,64 @@ struct AddGeulView: View {
                     .padding()
                     .overlay(
                         RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.gray, lineWidth: 1)
+                            .stroke(Color(red: 0.48, green: 0.49, blue: 0.50), lineWidth: 1) // 테두리 색상 변경
                     )
                 
-                Text("작가")
+                Text(selectedCategory == .lyrics ? "가수" : "지은이")
                     .font(.headline)
                 
-                TextField("작가를 입력하세요", text: $name)
+                TextField(selectedCategory == .lyrics ? "가수를 입력하세요" : "지은이를 입력하세요", text: $name)
                     .padding()
                     .overlay(
                         RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.gray, lineWidth: 1)
+                            .stroke(Color(red: 0.48, green: 0.49, blue: 0.50), lineWidth: 1) // 테두리 색상 변경
                     )
                 
                 Text("내용")
                     .font(.headline)
+
+                ZStack(alignment: .topLeading) {
+                    TextEditor(text: $content)
+                        .frame(height: 150)
+                        .padding(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color(red: 0.48, green: 0.49, blue: 0.50), lineWidth: 1) // 테두리 색상 변경
+                        )
+                    if content.isEmpty {
+                        Text("내용을 입력하세요")
+                            .foregroundColor(Color(UIColor.placeholderText)) // UIKit의 lightGray 색상 사용
+                            .font(.system(size: 17)) // 폰트 크기를 17로 설정
+                            .padding(.horizontal, 16)
+                            .padding(.vertical, 13)
+                    }
+
+
+                }
+
                 
-                TextEditor(text: $content)
-                    .padding()
-                    .frame(height: 150)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.gray, lineWidth: 1)
-                    )
             }
             .padding()
             
             Spacer()
             
-            Button(action: {
-                if let selectedCategory = selectedCategory {
-                    let newGeul = GeulInfo(content: content, name: name, category: selectedCategory)
-                    geul.append(newGeul)  // 새로운 글을 리스트에 추가
-                    path.removeLast()  // 현재 뷰를 닫고 이전 화면으로 돌아감
+            HStack {
+                Spacer()
+                Button(action: {
+                    if let selectedCategory = selectedCategory {
+                        let newGeul = GeulInfo(title: title, content: content, name: name, category: selectedCategory)
+                        geul.append(newGeul)  // 새로운 글을 리스트에 추가
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                }) {
+                    Text("등록하기")
+                        .foregroundColor(.black)
+                        .padding()
+                        .padding(.horizontal, 25)  // 양옆 여백 추가
+                        .background(Color.gray.opacity(0.3))
+                        .cornerRadius(15)
                 }
-            }) {
-                Text("등록하기")
-                    .foregroundColor(.black)
-                    .padding()
-                    .background(Color.gray.opacity(0.3))
-                    .cornerRadius(8)
+                Spacer()
             }
             
             Spacer()
--- badageul/badageul/View/EditGeulView.swift
+++ badageul/badageul/View/EditGeulView.swift
@@ -1,33 +1,14 @@
 import SwiftUI
 
 struct EditGeulView: View {
-    @Binding var path: NavigationPath
-    @Binding var geul: [GeulInfo]  // 상위 뷰로부터 전달된 글 리스트에 바인딩
-
-    @State private var selectedCategory: GeulInfo.Category? = nil
-    @State private var title: String = ""
-    @State private var name: String = ""
-    @State private var content: String = ""
-
+    @Binding var geul: GeulInfo  // 선택된 글귀의 바인딩
+    
     var body: some View {
-        VStack(spacing: 20) {
-            Spacer()
-            
-            Text("여기는 글수정 입니다.")
-            
-            Spacer()
-            
-            Button {
-                path.removeLast(path.count)
-            } label: {
-                Text("등록하기")
-            }
-            
-            Spacer()
+        Form {
+            TextField("Title", text: $geul.title)
+            TextField("Name", text: $geul.name)
+            TextEditor(text: $geul.content)
         }
+        .navigationTitle("Edit Geul")
     }
-}
-
-#Preview {
-    ContentView()
 }
